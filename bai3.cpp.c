436dae2eb86bd01ea2773926516576cf7aa11da3
anhpqchamp1 <quocanhtt10@gmail.com>
2024-12-07 16:09:51 +0700

    Test

---
+++ bai3.cpp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    float nhietDo;
+    printf("Nhap nhiet do (oC): ");
+    scanf("%f",hietDo >= 40) { &nhietDo);
+    
+    if (n
+        printf("Thoi tiet: Cuc nong\n");
+    } else if (nhietDo >= 30) {
+        printf("Thoi tiet: Nong\n");
+    } else if (nhietDo >= 20) {
+        printf("Thoi tiet: Mat me\n");
+    } else if (nhietDo >= 10) {
+        printf("Thoi tiet: Se lanh\n");
+    } else if (nhietDo >= 0) {
+        printf("Thoi tiet: Lanh\n");
+    } else {
+        printf("Thoi tiet: Rat lanh\n");
+    }
+
+    return 0;
+}
+
